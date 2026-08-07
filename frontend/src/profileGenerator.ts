380c0a4be3cf3448427301d3e9030d41ace8ebc5
Abdul Ahad Butt <abdulahadbutt@example.com>
2026-08-06 22:38:08 -0500

    Fix faker locale error and clean up old tailwind config files

--- frontend/src/profileGenerator.ts
+++ frontend/src/profileGenerator.ts
@@ -1,4 +1,4 @@
-import { faker } from '@faker-js/faker';
+import { fakerEN_US as faker } from '@faker-js/faker';
 import { US_STATE_AREA_CODES } from './usStateAreaCodes';
 import { validateNanpPhone, validateStateZip } from './usValidator';
 
